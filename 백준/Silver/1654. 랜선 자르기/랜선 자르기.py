99ba73a950479f774528f22e5d52d07e57bfdb40
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2022-12-05 17:13:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,16 +1,32 @@
 import sys
-K, N = map(int, input().split())
-lan = [int(sys.stdin.readline()) for _ in range(K)]
-start, end = 1, max(lan) #이분탐색 처음과 끝위치
 
-while start <= end: #적절한 랜선의 길이를 찾는 알고리즘
-    mid = (start + end) // 2 #중간 위치
-    lines = 0 #랜선 수
-    for i in lan:
-        lines += i // mid #분할 된 랜선 수
-        
-    if lines >= N: #랜선의 개수가 분기점
-        start = mid + 1
-    else:
-        end = mid - 1
-print(end)
+# had: 가지고 있는 랜선 개수,  need: 필요한 랜선 개수
+had, need = list(map(int, input().split()))
+
+lines = list()
+for _ in range(had):
+    lines.append(int(input()))
+# [int(input()) for _ in range(had)]
+
+# 만들 수 있는 최대 랜선 길이 구하기 
+left, right = 1, max(lines)
+
+while left <= right:
+    mid = (left + right) // 2
+    cnt = 0
+    for line in lines:
+        cnt += (line // mid)
+    if cnt < need:     # 자른 개수가 필요한거보다 작을때, right가 더 작아야함
+        right = mid - 1
+    elif cnt >= need:    # 자른 개수가 필요한거보다 많을 때, left가 더 커져야함
+        left = mid + 1
+    
+print(right)
+
+
+
+# 문제 : 기존의 K개의 랜선으로 N개의 랜선을 만들 수 없는 경우는 없다고 가정하자.
+# 문제 제대로 읽으면 아래와 같은 코드를 쓰면 안되는데,.
+# if had >= need:     
+#     print(min(lines))
+#     sys.exit()
