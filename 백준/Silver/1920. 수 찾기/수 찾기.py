db2554682910dc1b0654e7af8be2f69f99051673
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2022-12-05 17:46:07 +0900

    [Silver IV] Title: 수 찾기, Time: 1000 ms, Memory: 46408 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,25 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+n_list.sort()   # 이분 탐색을 위해 미리 정렬
+
+# m안에 있는 수가 n에 있는 지 확인
+for i in range(m):
+    left, right = 0, n
+
+    while True:
+        mid = (left + right) // 2
+        if left <= right and mid < n:
+            if n_list[mid] == m_list[i]:  
+                print(1)
+                break
+            elif n_list[mid] < m_list[i]:     # m의 요소가 n의 요소보다 크다면, right를 줄여야함
+                left = mid + 1
+            elif n_list[mid] > m_list[i]:
+                right = mid - 1
+        else:
+            print(0)
+            break
