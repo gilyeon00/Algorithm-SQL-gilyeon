5b1a79f6029a0f2c724a539aeca016fde93c8f27
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2022-12-13 12:49:58 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 616 ms, Memory: 216448 KB -BaekjoonHub

---
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+dp = [ [0] * (m+1)  for _ in range(n+1) ]
+dp[1][1] = 1
+
+for x in range(1, n+1):
+    for y in range(1, m+1):
+        if x == 1 and y ==1 :
+            continue
+        else:
+            dp[x][y] = dp[x-1][y] + dp[x-1][y-1] + dp[x][y-1]
+        
+result = dp[n][m]
+STANDARD = 1000000007
+
+if result < STANDARD:
+    print(result)
+else:
+    print(result%STANDARD)
