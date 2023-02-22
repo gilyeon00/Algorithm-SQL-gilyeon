54557dc506fe28e5f1caa3ddad8bd8ba3623143b
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2023-02-22 23:52:42 +0900

    [level 3] Title: 단속카메라, Time: 1.41 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,25 @@
+def solution(routes):
+    # 테스트 1은 맞지만 다른게 다 틀리고, 당연히 시간초과가 우려됨
+#     answer = 0
+#     cam_list = [0] * 60001
+    
+#     for route in routes:
+#         a,b = route
+#         a += 30000
+#         b += 30000
+#         for i in range(a, b+1):
+#             cam_list[i] += 1
+            
+#     answer = max(cam_list)
+    answer = 0
+    routes.sort(key=lambda x: x[1]) # 가장빨리 나온차부터 보려고
+    cam_list = [ routes[0][1] ]
+    
+    for i in range(1, len(routes)):
+        if routes[i][0] <= cam_list[-1] and routes[i][1] >= cam_list[-1]:   # 겹치는 부분 있을 때
+            continue
+        else:   # 겹치는 부분이 없을 때 
+            cam_list.append(routes[i][1])
+        
+    answer = len(cam_list)
+    return answer
