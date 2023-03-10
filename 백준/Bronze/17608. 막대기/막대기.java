11c03304b32c7e0e3d374e64f6ca49d9dbcb1bb8
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2023-03-10 14:06:37 +0900

    [Bronze II] Title: 막대기, Time: 300 ms, Memory: 25300 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -14,12 +17,12 @@ public class Main {
         }
         return cnt;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         Stack stickStack = new Stack();
         while(n > 0){
-            int stick = sc.nextInt();
+            int stick = Integer.parseInt(br.readLine());
             stickStack.push(stick);
             n --;
         }
