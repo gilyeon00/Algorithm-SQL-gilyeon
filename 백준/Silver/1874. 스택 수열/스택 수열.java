ca496ab2691c8ab1b492fcbddbb71c03e8c0030b
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2023-03-10 14:05:50 +0900

    [Silver II] Title: 스택 수열, Time: 1524 ms, Memory: 44440 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,13 +1,13 @@
-import java.sql.Array;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         Stack numStack = new Stack<>();
         ArrayList targetList = new ArrayList<>();
@@ -15,7 +15,7 @@ public class Main {
         ArrayList returnList = new ArrayList<>();
 
         for(int i =0; i<n; i++){
-            int inputNum = sc.nextInt();
+            int inputNum = Integer.parseInt(br.readLine());
             targetList.add(inputNum);
         }
 
