fe4b308a01bdf10939ddf472c08d5398ae9d1ee7
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2023-05-03 12:57:29 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 50404 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,25 +1,34 @@
-n = int(input())
-n_list = list(map(int, input().split()))
+n =  int(input())
+# target = list(map(int, input().split()))
+target = set(map(int, input().split()) )
 
 m = int(input())
-m_list = list(map(int, input().split()))
+check = list(map(int, input().split()))
 
-n_list.sort()   # 이분 탐색을 위해 미리 정렬
+# 1. 시간 초과 - in 사용
+# for a in check:
+#     if a in target:
+#         print(1)
+#         continue
+#     else : print(0)
 
-# m안에 있는 수가 n에 있는 지 확인
-for i in range(m):
-    left, right = 0, n
+# 2. 시간 초과
+# target.sort()
+# for i in range(len(check)):
+#     if check[i] > target[len(target)-1]:
+#         print(0)
+#         continue
+#     if check[i] < target[0]:
+#         print(0)
+#         continue
+#     if check[i] in target:
+#         print(1)
+#         continue
+#     else:
+#         print(0)
 
-    while True:
-        mid = (left + right) // 2
-        if left <= right and mid < n:
-            if n_list[mid] == m_list[i]:  
-                print(1)
-                break
-            elif n_list[mid] < m_list[i]:     # m의 요소가 n의 요소보다 크다면, right를 줄여야함
-                left = mid + 1
-            elif n_list[mid] > m_list[i]:
-                right = mid - 1
-        else:
-            print(0)
-            break
+
+for a in check:
+    if a in target:
+        print(1)
+    else : print(0)
