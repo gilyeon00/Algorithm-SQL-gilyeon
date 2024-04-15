61daf1d7b1c14faba1fc29910bb0808451d53282
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2024-04-16 01:30:15 +0900

    [level 0] Title: 문자열 섞기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,14 +1,8 @@
 def solution(str1, str2):
     answer = ''
-    if len(str1) > len(str2):
-        for i in range(len(str2)):
-            answer += str1[i]
-            answer += str2[i]
-        
-    else:
-        for i in range(len(str1)):
-            answer += str1[i]
-            answer += str2[i]
 
-    
+    for i in range(len(str1)):
+        answer += str1[i]
+        answer += str2[i]
+
     return answer
